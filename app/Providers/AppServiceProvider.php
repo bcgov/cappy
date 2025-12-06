e94e3b857252677b22241fc144f5646845c7be07
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-12-05 17:13:51 -0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        } else {
+            Model::preventLazyLoading(false);
+        }
     }
 }
