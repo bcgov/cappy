e3217a7829a4ef75fba64a0274901d69798e9615
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-12-09 13:19:01 -0800

    add debug route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/debug-host', function () {
+    dd([
+        'host' => request()->getHost(),
+        'scheme' => request()->getScheme(),
+        'full_url' => request()->fullUrl(),
+        'secure' => request()->isSecure(),
+    ]);
+});
