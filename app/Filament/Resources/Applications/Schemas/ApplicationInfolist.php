e1ff29fc90679b168242e78852ea6eb105f08126
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-12-09 10:05:51 -0800

    clean up display

--- app/Filament/Resources/Applications/Schemas/ApplicationInfolist.php
+++ app/Filament/Resources/Applications/Schemas/ApplicationInfolist.php
@@ -5,48 +5,59 @@ namespace App\Filament\Resources\Applications\Schemas;
 use App\Models\Application;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
 
 class ApplicationInfolist
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->inlineLabel()
+            ->columns(1)
             ->components([
-                TextEntry::make('name'),
-                TextEntry::make('ministry.name'),
-                TextEntry::make('division')
-                    ->placeholder('-'),
-                TextEntry::make('business_owner_name'),
-                TextEntry::make('business_owner_email'),
-                TextEntry::make('technical_contact_name'),
-                TextEntry::make('technical_contact_email'),
-                TextEntry::make('description')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('status'),
-                TextEntry::make('hosting_type')
-                    ->placeholder('-'),
-                TextEntry::make('hosting_details')
-                    ->placeholder('-'),
-                TextEntry::make('documentation_url')
-                    ->placeholder('-'),
-                TextEntry::make('repository_url')
-                    ->placeholder('-'),
-                TextEntry::make('go_live_date')
-                    ->date()
-                    ->placeholder('-'),
-                TextEntry::make('end_of_life_date')
-                    ->date()
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn(Application $record): bool => $record->trashed()),
+                Section::make('Basic Information')
+                    ->inlineLabel()
+                    ->schema([
+                        TextEntry::make('name'),
+                        TextEntry::make('ministry.name'),
+                        TextEntry::make('division')->placeholder('-'),
+                        TextEntry::make('description')
+                            ->placeholder('-')
+                            ->columnSpanFull(),
+                        TextEntry::make('status')
+                            ->formatStateUsing(fn($state) => Application::getStatusOptions()[$state] ?? $state),
+                    ]),
+                Section::make('Business Information')
+                    ->inlineLabel()
+                    ->schema([
+                        TextEntry::make('business_owner_name'),
+                        TextEntry::make('business_owner_email'),
+                    ]),
+                Section::make('Technical Information')
+                    ->inlineLabel()
+                    ->schema([
+                        TextEntry::make('technical_contact_name'),
+                        TextEntry::make('technical_contact_email'),
+                        TextEntry::make('hosting_type')
+                            ->placeholder('-')
+                            ->formatStateUsing(fn($state) => Application::getHostingTypeOptions()[$state] ?? $state),
+                        TextEntry::make('hosting_details')
+                            ->placeholder('-'),
+                    ]),
+                Section::make('Additional Information')
+                    ->inlineLabel()
+                    ->schema([
+                        TextEntry::make('documentation_url')
+                            ->placeholder('-'),
+                        TextEntry::make('repository_url')
+                            ->placeholder('-'),
+                        TextEntry::make('go_live_date')
+                            ->date()
+                            ->placeholder('-'),
+                        TextEntry::make('end_of_life_date')
+                            ->date()
+                            ->placeholder('-'),
+                    ]),
             ]);
     }
 }
