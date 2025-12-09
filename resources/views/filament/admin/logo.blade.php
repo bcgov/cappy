60f01409edccda7115fcb261ea844345db7815ed
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-12-09 09:33:45 -0800

    add text next to logo

---
+++ resources/views/filament/admin/logo.blade.php
@@ -0,0 +1,7 @@
+<div class="flex items-center gap-1.5">
+    <img
+        src="{{ asset('images/cappy.svg') }}"
+        alt="Cappy Logo"
+        style="height: 2rem; width: auto; display: inline-block; vertical-align: middle;">
+    <span class="text-lg font-semibold text-gray-800 dark:text-white" style="display: inline-block; vertical-align: middle;">Cappy</span>
+</div>
