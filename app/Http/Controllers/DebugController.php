86dfa952b533440302af09fabbadfa9e754db8fa
Jeremy Vernon <58346777+jeremy-vernon-bcgov@users.noreply.github.com>
2025-12-23 12:17:09 -0800

    Adding additional environment variables to allow debugging

---
+++ app/Http/Controllers/DebugController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role; // Import the Role model
+
+class DebugController extends Controller
+{
+    public function checkRole()
+    {
+        // This is a placeholder. The original content is not available.
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/clear-permission-cache', function() {
+    app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+    return 'Spatie permission cache cleared!';
+});
+
 Route::get('/debug-role-check', [DebugController::class, 'checkRole'])->middleware('web');
