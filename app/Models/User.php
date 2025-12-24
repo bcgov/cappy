8e47dcf1a8331b293f8f9b524643b686f17ed745
Jeremy Vernon <58346777+jeremy-vernon-bcgov@users.noreply.github.com>
2025-12-24 10:54:13 -0800

    Attempted fix of 403 issue
    Identified that Filament user interface was not marked as implemented in the user model - applying fix.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
